904c08eb7dbd5b7e861cd0146014637d1386431e
seeunpark <senasoon0@gmail.com>
2022-08-17 11:15:44 +0900

    todo 코드 간소화

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import "./style.css";
 
 function Todo(props) {
-  if (props.todo.isDone === true){
     return (
       <div className='todo'>
         <h3>{props.todo.title}</h3>
@@ -13,26 +12,11 @@ function Todo(props) {
           }}>삭제하기</button>
           <button type="button" onClick={()=>{
             props.onChangeHandler(props.todo.id)
-          }}>취소</button>
+          }}>{props.todo.isDone? "취소":"완료"}</button>
         </div>
       </div>
-    )
-  } else {
-    return (
-      <div className='todo'>
-        <h3>{props.todo.title}</h3>
-        <p>{props.todo.content}</p>
-        <div className='buttons'>
-          <button type="button" onClick={()=>{
-            props.onDeleteHandler(props.todo.id)
-          }}>삭제하기</button>
-          <button type="button" onClick={()=>{
-            props.onChangeHandler(props.todo.id)
-          }}>완료</button>
-        </div>
-      </div>
-    )
-  }
+    );
+
 }
 
 export default Todo
